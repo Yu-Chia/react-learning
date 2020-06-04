83b22aba821d2d25ed356f89c6fbee9b0eeed98b
Martin <l850614@gmail.com>
2020-06-04 16:45:59 +0800

    20200604 04:45pm

---
+++ tmp-0604/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MyTimerClass from './components/MyTimerClass'
+import MyTimerFunc from './components/MyTimerFunc'
+
+export default function App(props) {
+  return (
+    <>
+      <MyTimerClass />
+      <MyTimerFunc />
+    </>
+  )
+}
