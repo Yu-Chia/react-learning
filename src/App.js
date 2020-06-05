ad42c3883d0bc7743e681bca2d1a1881c1c109cf
Martin <l850614@gmail.com>
2020-06-05 14:17:43 +0800

    20200605 02:17 pm add react-touter

--- src/App.js
+++ src/App.js
@@ -1,89 +1,43 @@
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 import React, { useState, useEffect } from 'react'
+
 import MyNavbar from './components/MyNavbar'
 import MyFooter from './components/MyFooter'
-
-import Addform from './components/Addform'
-//import TodoItem from './components/TodoItem'
 import MainContent from './components/MainContent'
-import TodoList from './components/TodoList'
+
+import Home from './pages/Home'
+import About from './pages/About'
+import TodoApp from './pages/TodoApp'
+
+// import TodoAddForm from './components/todo/TodoAddForm'
+//import TodoItem from './components/TodoItem'
+// import TodoList from './components/todo/TodoList'
 
 function App() {
-  // todos列表
-  // todo = { id: 123213, text:'買牛奶', completed: false }
-  const [todos, setTodos] = useState([
-    { id: 1591256594282, text: '買牛奶', completed: false, edited: false },
-    { id: 1591256594281, text: '買iphone', completed: false, edited: true },
-    { id: 1591256594283, text: '學react', completed: false, edited: false },
-  ])
-
-  // 用於文字輸入框輸入新的todo
-  const [text, setText] = useState('')
-
-  // 利用id值尋找符合的todos裡的index，然後改變completed
-  const handleCompleted = (id) => {
-    const newTodos = [...todos]
-
-    const todoItemIndex = todos.findIndex((v, i) => v.id === id)
-
-    if (todoItemIndex !== -1) {
-      newTodos[todoItemIndex].completed = !newTodos[todoItemIndex].completed
-      setTodos(newTodos)
-    }
-  }
-
-  // 把成員直接從陣列中刪除
-  const handleDelete = (id) => {
-    const newTodos = todos.filter((v, i) => v.id !== id)
-
-    setTodos(newTodos)
-  }
-
-  // 利用id值尋找符合的todos裡的index，然後改變edited值
-  const handleEditedToggle = (id) => {
-    const newTodos = [...todos]
-
-    const todoItemIndex = todos.findIndex((v, i) => v.id === id)
-
-    if (todoItemIndex !== -1) {
-      newTodos[todoItemIndex].edited = !newTodos[todoItemIndex].edited
-      setTodos(newTodos)
-    }
-  }
-
-  // 利用id值尋找符合的todos裡的index，然後改變text值
-  const handleEditedSave = (id, text) => {
-    const newTodos = [...todos]
-
-    const todoItemIndex = todos.findIndex((v, i) => v.id === id)
-
-    if (todoItemIndex !== -1) {
-      newTodos[todoItemIndex].text = text
-      setTodos(newTodos)
-    }
-
-    handleEditedToggle(id)
-  }
-
   return (
-    <>
-      <MyNavbar />
-      <MainContent title="待辨事項">
-        <Addform
-          text={text}
-          todos={todos}
-          setText={setText}
-          setTodos={setTodos}
-        />
-        <TodoList
-          todos={todos}
-          handleCompleted={handleCompleted}
-          handleDelete={handleDelete}
-          handleEditedToggle={handleEditedToggle}
-          handleEditedSave={handleEditedSave}
-        />
-      </MainContent>
-      <MyFooter />
-    </>
+    <Router>
+      <>
+        <MyNavbar />
+        <MainContent title="1213">
+          <Link to="/">首頁</Link>
+          <Link to="/about">關於</Link>
+          <Link to="/todoapp">待辨</Link>
+
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/todoapp">
+              <TodoApp />
+            </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </MainContent>
+        <MyFooter />
+      </>
+    </Router>
   )
 }
 
